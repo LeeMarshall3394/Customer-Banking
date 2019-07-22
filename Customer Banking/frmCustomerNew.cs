9b692126ec8db8f1159a06ba32eaee56da314606
Lee Marshall <34558251+LeeMarshall3394@users.noreply.github.com>
2019-07-22 03:59:16 +0100

    Basic validation for the new customer form
    Wrote a function to check if a textbox is empty and show a message if it isn't.
    
    Used this function to perform basic validation on all strings used to add a new customer.
    
    Also ensured that allowance can only be an integer.

--- Customer Banking/frmCustomerNew.cs
+++ Customer Banking/frmCustomerNew.cs
@@ -30,47 +30,76 @@ namespace Assignment_2
             cboDate3.SelectedIndex = 0;
         }
 
-        private void btnNext_Click(object sender, EventArgs e)
+        private bool checkTextBox(TextBox x, string name)
         {
-            try
+            if (x.Text == "")
             {
-                //Open connection
-                myConn.Open();
-                OleDbCommand myCmd = myConn.CreateCommand();
-                //SQL code to enter a new customer into the database
-                myCmd.CommandText = @"INSERT INTO customer(title, firstname, lastname, dob, natins, email, pswd, allowance)
-                                         VALUES(@gettitle, @getfirstname, @Getlastname, @getdob, @getnatins, @getemail, @getpswd, @getallowance);";
-
-                //Parameter passing to avoid SQL injection
-                myCmd.Parameters.AddWithValue("Gettitle", cboTitle.Text);
-                myCmd.Parameters.AddWithValue("getfirstname", txtFirstName.Text);
-                myCmd.Parameters.AddWithValue("getlastname", txtLastName.Text);
-                myCmd.Parameters.AddWithValue("getdob", cboDate1.Text + "/" + (cboDate2.SelectedIndex + 1) + "/" + cboDate3.Text);
-                myCmd.Parameters.AddWithValue("getnatins", txtNatIns.Text);
-                myCmd.Parameters.AddWithValue("getemail", txtEmail.Text);
-                myCmd.Parameters.AddWithValue("getpassword", txtPassword.Text);
-                myCmd.Parameters.AddWithValue("getallowance", txtAllowance.Text);
-
-                int result = myCmd.ExecuteNonQuery();
-
-                //if SQL executed properly
-                if (result == 1)
-                {
-                    //Message box saying records added
-                    MessageBox.Show(result + " record(s) added");
-                }
-                //Close the connection
-                myConn.Close();
+                MessageBox.Show(name + " has not been filled out.");
+                return false;
             }
-            //Catch any errors
-            catch (Exception ex)
-            {
-                //Show what the error was
-                MessageBox.Show(ex.Message);
-                //Close the connection
-                myConn.Close();
-            }
+            else return true;
         }
 
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            int myInt;
+            if (checkTextBox(txtFirstName, "First name"))
+            {
+                if (checkTextBox(txtLastName, "Last name"))
+                {
+                    if (checkTextBox(txtNatIns, "Nat ins"))
+                    {
+                        if (checkTextBox(txtEmail, "Email"))
+                        {
+                            if (checkTextBox(txtPassword, "Password"))
+                            {
+                                if (int.TryParse(txtAllowance.Text, out myInt))
+                                {
+                                    try
+                                    {
+                                        //Open connection
+                                        myConn.Open();
+                                        OleDbCommand myCmd = myConn.CreateCommand();
+                                        //SQL code to enter a new customer into the database
+                                        myCmd.CommandText = @"INSERT INTO customer(title, firstname, lastname, dob, natins, email, pswd, allowance)
+                                         VALUES(@gettitle, @getfirstname, @Getlastname, @getdob, @getnatins, @getemail, @getpswd, @getallowance);";
+
+                                        //Parameter passing to avoid SQL injection
+                                        myCmd.Parameters.AddWithValue("Gettitle", cboTitle.Text);
+                                        myCmd.Parameters.AddWithValue("getfirstname", txtFirstName.Text);
+                                        myCmd.Parameters.AddWithValue("getlastname", txtLastName.Text);
+                                        myCmd.Parameters.AddWithValue("getdob", cboDate1.Text + "/" + (cboDate2.SelectedIndex + 1) + "/" + cboDate3.Text);
+                                        myCmd.Parameters.AddWithValue("getnatins", txtNatIns.Text);
+                                        myCmd.Parameters.AddWithValue("getemail", txtEmail.Text);
+                                        myCmd.Parameters.AddWithValue("getpassword", txtPassword.Text);
+                                        myCmd.Parameters.AddWithValue("getallowance", txtAllowance.Text);
+
+                                        int result = myCmd.ExecuteNonQuery();
+
+                                        //if SQL executed properly
+                                        if (result == 1)
+                                        {
+                                            //Message box saying records added
+                                            MessageBox.Show(result + " record(s) added");
+                                        }
+                                        //Close the connection
+                                        myConn.Close();
+                                    }
+                                    //Catch any errors
+                                    catch (Exception ex)
+                                    {
+                                        //Show what the error was
+                                        MessageBox.Show(ex.Message);
+                                        //Close the connection
+                                        myConn.Close();
+                                    }
+                                }
+                                else MessageBox.Show("Allowance hasn't been filled correctly");
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
